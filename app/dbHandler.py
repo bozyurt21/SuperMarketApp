a61d0c25f89538abdab17c856a0f926dcdadebed
bensu ozyurt <100310215+bozyurt21@users.noreply.github.com>
2025-05-22 14:39:28 +0300

    Created dbHandler class for performing basic functions although create table must have done individually

---
+++ app/dbHandler.py
@@ -0,0 +1,30 @@
+import mysql.connector
+import pandas as pd
+import csv
+class dbHandler:
+  def self (__init__):
+    db_connection = mysql.connector.connect(
+                      host="localhost",
+                      user="root",
+                      passwd="<your-database-password>", # Change only this 
+                      auth_plugin='mysql_native_password'
+       
+               )
+    db_cursor = db_connection.cursor(buffered=True)
+  
+  # Create and connect to the database
+  def createDatabase(self, databaseName):
+    query1 = "CREATE DATABASE " + databaseName 
+    query2 = "USE " + databaseName 
+    self.db_cursor.execute(query1)
+    self.db_cursor.execute(query2)
+  
+  #Delete Database
+  def deleteDatabase(self, databaseName):
+    query = "DROP DATABASE "+databaseName
+    self.db_cursor.execute(query)
+  
+  #Delete Table
+  def deleteTable(self, tableName):
+    query = "DROP TABLE "+ tableName
+    self.db_cursor.execute(query)
