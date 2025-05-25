8374812fad3612f62e502fe038cb9e540e402585
bensu ozyurt <100310215+bozyurt21@users.noreply.github.com>
2025-05-25 21:05:47 +0300

    Changed the method in the form so I can send it to flask and also added login method to flask but implementation is going to wait a little since we do not have customer table yet

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import mysql.connector
 
 app = Flask(__name__)
@@ -10,3 +10,18 @@ def get_connection():
         password="yourpassword",
         database="yourdatabase"
     )
+
+# Added db connection and cursor to connect the code to the db
+db_connection = get_connection()
+db_cursor = db_connection.cursor(buffered=True)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        # Add function to get email and password from the user
+
+        
+        
